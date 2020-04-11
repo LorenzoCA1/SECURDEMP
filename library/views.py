aff0c50a7b0fdbbb5c684aa84d86c2009b55fe53
lorenzoavancena <lorenzo>
2020-04-11 22:00:26 +0800

    Merge branch 'master' of https://github.com/LorenzoCA1/SECURDEMP

--- library/views.py
+++ library/views.py
@@ -85,15 +85,15 @@ class BookInstanceUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView
 	model = BookInstance
 	fields = ['imprint','status']
 
-	#def form_valid(self, form):
-		#book = Book.objects.get(pk=self.kwargs['pk'])
-		#form.instance.book = book
-		#return super(BookInstanceUpdateView, self).form_valid(form)
+	def form_valid(self, form):
+		book = Book.objects.get(pk=self.kwargs['pk'])
+		form.instance.book = book
+		return super(BookInstanceUpdateView, self).form_valid(form)
 
-	#def form_valid(self, form):
-		#bookInstance = BookInstance.objects.get(pk=self.kwargs['bookinstance_id'])
-		#form.instance.bookInstance = bookInstance
-		#return super(BookInstanceUpdateView, self).form_valid(form)
+	def form_valid(self, form):
+		id = BookInstance.objects.get(pk=self.kwargs['bookinstance_id'])
+		form.instance.id = id
+		return super(BookInstanceUpdateView, self).form_valid(form)
 
 	def test_func(self):
 		if str(self.request.user.profile.Role) == "Book Manager":
